def5dc85f651c1d22a484491e727b1b77e19121c
Joe Duhamel <jduhamel@gmail.com>
2023-01-25 20:54:00 -0500

    Update bufferline and flutter_tools

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -1,167 +1,203 @@
-local status_ok, bufferline = pcall(require, "bufferline")
-if not status_ok then
-  return
+local M = {}
+
+M.config = function()
+  local kind = require "user.lsp_kind"
+  lvim.builtin.bufferline.highlights = {
+    background = { italic = true },
+    buffer_selected = { bold = true },
+  }
+  if lvim.builtin.time_based_themes then
+    lvim.builtin.bufferline.highlights.fill = {
+      bg = {
+        attribute = "bg",
+        highlight = "NormalNC",
+      },
+    }
+  end
+  local g_ok, bufferline_groups = pcall(require, "bufferline.groups")
+  if not g_ok then
+    bufferline_groups = {
+      builtin = {
+        pinned = {
+          name = "pinned",
+          with = function(_ico)
+          end,
+        },
+        ungroupued = { name = "ungrouped" },
+      },
+    }
+  end
+  lvim.builtin.bufferline.options = {
+    navigation = { mode = "uncentered" },
+    diagnostics = false, -- do not show diagnostics in bufferline
+    diagnostics_indicator = function(_, _, diagnostics)
+      local result = {}
+      local symbols = { error = kind.icons.error, warning = kind.icons.warn, info = kind.icons.info }
+      for name, count in pairs(diagnostics) do
+        if symbols[name] and count > 0 then
+          table.insert(result, symbols[name] .. count)
+        end
+      end
+      local res = table.concat(result, " ")
+      return #res > 0 and res or ""
+    end,
+
+    mode = "buffers",
+    sort_by = "insert_after_current",
+    always_show_bufferline = false,
+    groups = {
+      options = {
+        toggle_hidden_on_enter = true,
+      },
+      items = {
+--        bufferline_groups.builtin.pinned:with { icon = "" },
+        bufferline_groups.builtin.ungrouped,
+        {
+          name = "Dependencies",
+          icon = kind.icons.config,
+          highlight = { fg = "#ECBE7B" },
+          matcher = function(buf)
+            return vim.startswith(buf.path, string.format("%s/site/pack/lazy", vim.fn.stdpath "data"))
+              or vim.startswith(buf.path, vim.fn.expand "$VIMRUNTIME")
+          end,
+        },
+        {
+          highlight = { sp = "#51AFEF" },
+          name = "tests",
+          icon = kind.icons.test,
+          matcher = function(buf)
+            local name = buf.filename
+            return name:match "_spec" or name:match "_test" or name:match "test_"
+          end,
+        },
+        {
+          name = "Terraform",
+          matcher = function(buf)
+            return buf.name:match "%.tf" ~= nil
+          end,
+        },
+        {
+          name = "SQL",
+          matcher = function(buf)
+            return buf.filename:match "%.sql$"
+          end,
+        },
+        {
+          name = "view models",
+          highlight = { sp = "#03589C" },
+          matcher = function(buf)
+            return buf.filename:match "view_model%.dart"
+          end,
+        },
+        {
+          name = "screens",
+          icon = kind.icons.screen,
+          matcher = function(buf)
+            return buf.path:match "screen"
+          end,
+        },
+        {
+          highlight = { sp = "#C678DD" },
+          name = "docs",
+          matcher = function(buf)
+            for _, ext in ipairs { "md", "txt", "org", "norg", "wiki" } do
+              if ext == vim.fn.fnamemodify(buf.path, ":e") then
+                return true
+              end
+            end
+          end,
+        },
+        {
+          highlight = { sp = "#F6A878" },
+          name = "config",
+          matcher = function(buf)
+            local filename = buf.filename
+            if filename == nil then
+              return false
+            end
+            return filename:match "go.mod"
+              or filename:match "go.sum"
+              or filename:match "Cargo.toml"
+              or filename:match "manage.py"
+              or filename:match "Makefile"
+          end,
+        },
+      },
+    },
+    hover = { enabled = true, reveal = { "close" } },
+    offsets = {
+      {
+        text = "EXPLORER",
+        filetype = "neo-tree",
+        highlight = "PanelHeading",
+        text_align = "left",
+        separator = true,
+      },
+      {
+        text = " FLUTTER OUTLINE",
+        filetype = "flutterToolsOutline",
+        highlight = "PanelHeading",
+        separator = true,
+      },
+      {
+        text = "UNDOTREE",
+        filetype = "undotree",
+        highlight = "PanelHeading",
+        separator = true,
+      },
+      {
+        text = " LAZY",
+        filetype = "lazy", 
+        highlight = "PanelHeading",
+        separator = true,
+      },
+      {
+        text = " DATABASE VIEWER",
+        filetype = "dbui",
+        highlight = "PanelHeading",
+        separator = true,
+      },
+      {
+        text = " DIFF VIEW",
+        filetype = "DiffviewFiles",
+        highlight = "PanelHeading",
+        separator = true,
+      },
+    },
+    separator_style = vim.env.KITTY_WINDOW_ID and "slant" or "thin",
+    right_mouse_command = "vert sbuffer %d",
+    show_close_icon = false,
+    -- indicator = { style = "bold" },
+    indicator = {
+      icon = "▎", -- this should be omitted if indicator style is not 'icon'
+      style = "icon", -- can also be 'underline'|'none',
+    },
+    max_name_length = 18,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    truncate_names = true, -- whether or not tab names should be truncated
+    tab_size = 18,
+    color_icons = true,
+    show_buffer_close_icons = true,
+    diagnostics_update_in_insert = false,
+  }
 end
 
-bufferline.setup {
-  options = {
-    numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-    close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-    -- NOTE: this plugin is designed with this icon in mind,
-    -- and so changing this is NOT recommended, this is intended
-    -- as an escape hatch for people who cannot bear it for whatever reason
-    indicator_icon = "▎",
-    buffer_close_icon = "",
-    -- buffer_close_icon = '',
-    modified_icon = "●",
-    close_icon = "",
-    -- close_icon = '',
-    left_trunc_marker = "",
-    right_trunc_marker = "",
-    --- name_formatter can be used to change the buffer's label in the bufferline.
-    --- Please note some names can/will break the
-    --- bufferline so use this at your discretion knowing that it has
-    --- some limitations that will *NOT* be fixed.
-    -- name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-    --   -- remove extension from markdown files for example
-    --   if buf.name:match('%.md') then
-    --     return vim.fn.fnamemodify(buf.name, ':t:r')
-    --   end
-    -- end,
-    max_name_length = 30,
-    max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-    tab_size = 21,
-    diagnostics = false, -- | "nvim_lsp" | "coc",
-    diagnostics_update_in_insert = false,
-    -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
-    --   return "("..count..")"
-    -- end,
-    -- NOTE: this will be called a lot so don't do any heavy processing here
-    -- custom_filter = function(buf_number)
-    --   -- filter out filetypes you don't want to see
-    --   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-    --     return true
-    --   end
-    --   -- filter out by buffer name
-    --   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-    --     return true
-    --   end
-    --   -- filter out based on arbitrary rules
-    --   -- e.g. filter out vim wiki buffer from tabline in your work repo
-    --   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-    --     return true
-    --   end
-    -- end,
-    offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-    show_buffer_icons = true,
-    show_buffer_close_icons = true,
-    show_close_icon = true,
-    show_tab_indicators = true,
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    -- can also be a table containing 2 custom separators
-    -- [focused and unfocused]. eg: { '|', '|' }
-    separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
-    always_show_bufferline = true,
-    -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-    --   -- add custom logic
-    --   return buffer_a.modified > buffer_b.modified
-    -- end
-  },
-  highlights = {
-    fill = {
-      guifg = { attribute = "fg", highlight = "#ff0000" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-    background = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
+M.delete_buffer = function()
+  local fn = vim.fn
+  local cmd = vim.cmd
+  local buflisted = fn.getbufinfo { buflisted = 1 }
+  local cur_winnr, cur_bufnr = fn.winnr(), fn.bufnr()
+  if #buflisted < 2 then
+    cmd "confirm qall"
+    return
+  end
+  for _, winid in ipairs(fn.getbufinfo(cur_bufnr)[1].windows) do
+    cmd(string.format("%d wincmd w", fn.win_id2win(winid)))
+    cmd(cur_bufnr == buflisted[#buflisted].bufnr and "bp" or "bn")
+  end
+  cmd(string.format("%d wincmd w", cur_winnr))
+  local is_terminal = fn.getbufvar(cur_bufnr, "&buftype") == "terminal"
+  cmd(is_terminal and "bd! #" or "silent! confirm bd #")
+end
 
-    -- buffer_selected = {
-    --   guifg = {attribute='fg',highlight='#ff0000'},
-    --   guibg = {attribute='bg',highlight='#0000ff'},
-    --   gui = 'none'
-    --   },
-    buffer_visible = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-
-    close_button = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-    close_button_visible = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-    -- close_button_selected = {
-    --   guifg = {attribute='fg',highlight='TabLineSel'},
-    --   guibg ={attribute='bg',highlight='TabLineSel'}
-    --   },
-
-    tab_selected = {
-      guifg = { attribute = "fg", highlight = "Normal" },
-      guibg = { attribute = "bg", highlight = "Normal" },
-    },
-    tab = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-    tab_close = {
-      -- guifg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
-      guifg = { attribute = "fg", highlight = "TabLineSel" },
-      guibg = { attribute = "bg", highlight = "Normal" },
-    },
-
-    duplicate_selected = {
-      guifg = { attribute = "fg", highlight = "TabLineSel" },
-      guibg = { attribute = "bg", highlight = "TabLineSel" },
-      gui = "italic",
-    },
-    duplicate_visible = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-      gui = "italic",
-    },
-    duplicate = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-      gui = "italic",
-    },
-
-    modified = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-    modified_selected = {
-      guifg = { attribute = "fg", highlight = "Normal" },
-      guibg = { attribute = "bg", highlight = "Normal" },
-    },
-    modified_visible = {
-      guifg = { attribute = "fg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-
-    separator = {
-      guifg = { attribute = "bg", highlight = "TabLine" },
-      guibg = { attribute = "bg", highlight = "TabLine" },
-    },
-    separator_selected = {
-      guifg = { attribute = "bg", highlight = "Normal" },
-      guibg = { attribute = "bg", highlight = "Normal" },
-    },
-    -- separator_visible = {
-    --   guifg = {attribute='bg',highlight='TabLine'},
-    --   guibg = {attribute='bg',highlight='TabLine'}
-    --   },
-    indicator_selected = {
-      guifg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-      guibg = { attribute = "bg", highlight = "Normal" },
-    },
-  },
-}
+return M
